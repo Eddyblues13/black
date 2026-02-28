5bce6be61d8fb747175d2f74d1270674e461dd76
blueswayne133 <blueswayne133@gmail.com>
2026-03-01 04:16:05 +0700

    updates

---
+++ restore.bat
@@ -0,0 +1,8 @@
+git checkout 6d49207 -- "public/assets/logo.png"
+git checkout 6d49207 -- "public/temp/front/assets/img/logo.png"
+git checkout 6d49207 -- "public/temp/front/assets/img/logo-light.png"
+git checkout 6d49207 -- "public/temp/images/logo-dark.png"
+git checkout 6d49207 -- "public/themes/purposeTheme/temp/images/logo-dark.png"
+git checkout 6d49207 -- "public/storage/photos/k92TWOJNSymVqKfQEzVEPdfRy5kBcnm1r321cbcC.png"
+git checkout 6d49207 -- "storage/app/public/photos/k92TWOJNSymVqKfQEzVEPdfRy5kBcnm1r321cbcC.png"
+git status --short
